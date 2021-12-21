f475cff4cd011dccc7260fd9cbf48a29559db3e9
zeke <yangsin1031@naver.com>
2021-12-22 00:02:18 +0900

    프로그래머스: K번째수

--- Programmers/K번째수.swift
+++ Programmers/K번째수.swift
@@ -1,16 +1,7 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    var array1 = [Int]()
-    var answer = [Int]()
-    for i in 0..<commands.count {
-            for x in commands[i][0]-1...commands[i][1]-1 {
-                array1.append(array[x])
-            }
-        print(array1)
-        array1.sort()
-        answer.append(array1[commands[i][2]-1])
-        array1.removeAll()
+    return commands.map { command -> Int in
+        array[command[0]-1...command[1]-1].sorted(by: <)[command[2]-1]
     }
-    return answer
 }
