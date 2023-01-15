2ba732c62a2db868863561298b42824bf1083680
Zeke <yangsin1031@naver.com>
2023-01-16 00:10:08 +0900

    프로그래머스: 콜라 문제

--- Programmers/콜라 문제.swift
+++ Programmers/콜라 문제.swift
@@ -1,6 +1,16 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
-  
-    return 0
+  return recursion(a, b, n, cokeNum: 0)
+}
+
+func recursion(_ a:Int, _ b:Int, _ n:Int, cokeNum: Int) -> Int {
+  var n = n
+  if n < a {
+    return cokeNum
+  }
+  var emptyBottleCount = (n / a) * a
+  var newBottleCount = (n / a) * b
+  n = n - emptyBottleCount + newBottleCount
+  return recursion(a, b, n, cokeNum: cokeNum+newBottleCount)
 }
