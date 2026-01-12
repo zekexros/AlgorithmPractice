8b3da1850d080321a889ec9b23e85a08492a39c5
YangJoonHyeok <zzing1031@gmail.com>
2026-01-12 16:32:06 +0900

    BOJ: 약수 구하기

---
+++ BOJ/약수_구하기.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+
+count = 0
+
+for i in range(1, N + 1):
+    if N % i == 0:
+        count += 1
+        if count == K:
+            print(i)
+            break
+else:
+    print(0)
